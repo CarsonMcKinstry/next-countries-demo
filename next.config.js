7d81f7c60932e5768f0c045492e5c7ca92e44bfa
Carson McKinstry <carson.mckinstry@trivago.com>
2021-06-22 13:18:15 +0200

    derp

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const withGraphql = require("next-plugin-graphql");
 
 module.exports = withGraphql({
   reactStrictMode: true,
-  basePath: "next-countries-demo",
-  assetPrefix: "next-countries-demo",
+  basePath: "/next-countries-demo",
+  assetPrefix: "/next-countries-demo",
 });
